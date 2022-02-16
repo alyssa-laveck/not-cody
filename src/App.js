a6d2ff9403f66e3cb08ca658e065e56ff26b5c6f
acantrell <acantrell@osv.com>
2022-02-15 23:57:36 -0500

    set background-color and create initial GameTile component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
+import GameTile from './components/game-tile.tsx';
 import './App.css';
 
 function App() {
   return (
-    <div className="not-cody-class">Not Cody</div>
+    <>
+      <div className="not-cody-class">Not Cody</div>
+      <GameTile />
+    </>
   );
 }
 
---
+++ src/components/game-tile.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+const GameTile: FC = () => {
+    return (
+        <div className="game-tile"></div>
+    );
+};
+export default GameTile;
