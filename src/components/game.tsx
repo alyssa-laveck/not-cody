f25caf99e3b0ebca64e82a2a0e2b387321cba300
nlaveck <nlaveck@gmail.com>
2022-02-20 20:49:02 -0500

    Adds keyboard input logic to game component

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,11 +1,31 @@
-import { FC } from 'react';
+import { FC, useEffect, useState, useCallback } from 'react';
 import GameRow from './game-row.tsx';
 
 // type GameProps = {
 // };
 
 const Game: FC = () => {
+    const [input, setInput] = useState('');
+
     const ROW_COUNT = 6;
+    const WORD_LENGTH = 5;
+
+    useEffect(() => {
+        const onKeyDown = ({key }) => {
+            // console.log(key);
+            if (key === "Backspace") {
+                setInput(input.slice(0, input.length - 1))
+            }
+            else if (input.length < WORD_LENGTH) {
+                setInput(input + key);
+            }
+        };
+
+        window.addEventListener("keydown", onKeyDown);
+        return () => {
+            window.removeEventListener("keydown", onKeyDown);
+        }
+    }, [input, setInput, WORD_LENGTH]);
 
     const renderRows = (count) => {
         let rows = [];
@@ -15,12 +35,15 @@ const Game: FC = () => {
         return rows;
     };
 
-
+    console.log(input);
+    
     return (
     <div className="flex-center column">
         { renderRows(ROW_COUNT) }
     </div>
     );
+
+
 };
 
 export default Game;
