dd08bfc23a8f86498374bc98e5ede7400201de81
Robert Kinney <robertkinney@Roberts-MBP.home>
2022-02-24 18:50:25 -0500

    pushing to migrate

--- src/components/game-row.tsx
+++ src/components/game-row.tsx
@@ -3,11 +3,17 @@ import GameTile from './game-tile.tsx';
 
 const ROW_LENGTH = 5;
 
-const GameRow: FC = () => {
+type RowProps = {
+    isInputRow: boolean;
+    input: string;
+}
+
+const GameRow: FC<RowProps> = ({isInputRow, input}) => {
     let gameTiles: GameTile = [];
+    let inputWord = isInputRow ? input : '';
 
     for (let i = 0; i < ROW_LENGTH; i++) {
-        gameTiles.push(<GameTile key={i} letter="" status="good" />);
+        gameTiles.push(<GameTile key={i} letter={inputWord.charAt(i)} status="good" />);
     }
 
     return <div className="flex-center">{gameTiles}</div>;
--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,24 +1,40 @@
-import { FC, useEffect, useState, useCallback } from 'react';
+import { FC, useEffect, useState } from 'react';
 import GameRow from './game-row.tsx';
 
 // type GameProps = {
 // };
 
+const isValidWord = (word) => {
+    if (word.length < 5) {
+        return false;
+    }
+
+    // Need something to check if valid english word and add check here
+
+    return true;
+}
+
 const Game: FC = () => {
     const [input, setInput] = useState('');
+    const [currentRow, setRow] = useState(0);
 
     const ROW_COUNT = 6;
     const WORD_LENGTH = 5;
 
     useEffect(() => {
         const onKeyDown = ({ key, keyCode }) => {
-            // console.log(key);
             if (key === 'Backspace' && input.length > 0) {
                 setInput(input.slice(0, input.length - 1));
-            } else if (input.length < WORD_LENGTH) {
-                if (keyCode >= 65 && keyCode <= 90) {
-                    setInput(input + key.toUpperCase());
+            }
+
+            if (key === 'Enter' && isValidWord(input)) {
+                if (currentRow < ROW_COUNT - 1) {
+                    setRow(currentRow + 1);
                 }
+            }
+
+            if (keyCode >= 65 && keyCode <= 90 && input.length < WORD_LENGTH) {
+                setInput(input + key.toUpperCase());
             }
         };
 
@@ -27,13 +43,13 @@ const Game: FC = () => {
         return () => {
             window.removeEventListener('keydown', onKeyDown);
         };
-    }, [input, setInput, WORD_LENGTH]);
+    }, [input, setInput, WORD_LENGTH, currentRow]);
 
-    const renderRows = (count) => {
+    const renderRows = (count, currentRow) => {
         let rows = [];
 
         for (let i = 0; i < count; i++) {
-            rows.push(<GameRow key={i} />);
+            rows.push(<GameRow key={i} isInputRow={i === currentRow} input={input} />);
         }
 
         return rows;
@@ -41,7 +57,11 @@ const Game: FC = () => {
 
     console.log(input);
 
-    return <div className="flex-center column">{renderRows(ROW_COUNT)}</div>;
+    return (
+        <div className="flex-center column">
+            {renderRows(ROW_COUNT, currentRow)}
+        </div>
+    );
 };
 
 export default Game;
