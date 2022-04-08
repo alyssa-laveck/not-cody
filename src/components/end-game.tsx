be75d2c2f4f5b16c53cea30175de5ba7ee3ffde7
nlaveck <nlaveck@gmail.com>
2022-04-07 22:29:58 -0400

    Finishes game logic using word lists

--- src/components/end-game.tsx
+++ src/components/end-game.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
 
-const EndGame: FC<{ isWinner: boolean }> = ({ isWinner }) => {
+interface EndGameProps {
+    isWinner: boolean;
+    correctWord: string;
+}
+
+const EndGame: FC<EndGameProps> = ({ isWinner, correctWord }) => {
     const winningText = 'Winner Winner, Chicken Dinner!';
-    const losingText = 'Loser Loser, Chicken Snoozer!';
+    const losingText = `Loser Loser, Chicken Snoozer! The correct word is "${correctWord}".`;
 
     const gameResult = isWinner ? winningText : losingText;
 
-    return <div className="end-game-container">
+    return <div className="container">
         {gameResult}
     </div>;
 };
