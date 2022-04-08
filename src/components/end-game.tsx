352792a46eb981a0b81a62ed6277435e92e733c5
nlaveck <nlaveck@gmail.com>
2022-04-07 21:38:37 -0400

    Adds losing state

--- src/components/end-game.tsx
+++ src/components/end-game.tsx
@@ -1,6 +1,13 @@
 import { FC } from 'react';
 
-const EndGame: FC = () => {
-    return <div className="end-game-container">Winner Winner, Chicken Dinner!</div>;
+const EndGame: FC<{ isWinner: boolean }> = ({ isWinner }) => {
+    const winningText = 'Winner Winner, Chicken Dinner!';
+    const losingText = 'Loser Loser, Chicken Snoozer!';
+
+    const gameResult = isWinner ? winningText : losingText;
+
+    return <div className="end-game-container">
+        {gameResult}
+    </div>;
 };
 export default EndGame;
